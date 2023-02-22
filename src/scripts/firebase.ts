4e57f76b78a8f99b1dafa9b3dc0388d3d325da19
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:06:19 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDw2ssiqGAC1rqPGlNY2-49Xowx7IzhEa0",
-	appId: "1:742384357186:web:89760157292cbcf091c2db",
-	authDomain: "nikola-hristov.firebaseapp.com",
-	measurementId: "G-YJD003JSKY",
-	messagingSenderId: "742384357186",
-	projectId: "nikola-hristov",
-	storageBucket: "nikola-hristov.appspot.com",
+	apiKey: "AIzaSyCiIOIiAvdRwC5n6AzX_F5WSivt9bvosTQ",
+	authDomain: "nikolahristov-web.firebaseapp.com",
+	projectId: "nikolahristov-web",
+	storageBucket: "nikolahristov-web.appspot.com",
+	messagingSenderId: "540449914377",
+	appId: "1:540449914377:web:bf0a39a757e9ebcd1ec2d8",
+	measurementId: "G-146FB9N18L",
 };
 
 initializeApp(firebaseConfig);
