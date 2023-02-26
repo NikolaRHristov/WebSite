165aaef8d8593e7d3b3d5b14fb2ef4007ae4ed37
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-26 15:25:29 +0200

    squash!

--- module/astro-rome/src/options/rome.ts
+++
@@ -1,8 +0,0 @@
-import type { Configuration } from "@rometools/wasm-nodejs";
-
-export interface ROME extends Configuration {
-	// rome-ignore lint/suspicious/noExplicitAny:
-	[key: string]: any;
-}
-
-export default {} satisfies ROME;
