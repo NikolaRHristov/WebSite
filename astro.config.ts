d3192319b2c637127207dccd807f118d50b50414
Nikola Hristov <nikola@nikolahristov.tech>
2023-04-18 23:57:23 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -5,19 +5,11 @@ import compress from "astro-compress";
 import critters from "astro-critters";
 import rome from "astro-rome";
 import { defineConfig } from "astro/config";
-import worker from "astrojs-service-worker";
 
 export default defineConfig({
 	site: "https://nikolahristov.tech",
 	integrations: [
-		import.meta.env.MODE === "production"
-			? worker({
-					workbox: {
-						cleanupOutdatedCaches: true,
-						cacheId: "53",
-					},
-			  })
-			: null,
+		// import.meta.env.MODE === "production" ? worker() : null,
 		sitemap(),
 		critters({ logger: 1 }),
 		prefetch(),
