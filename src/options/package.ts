14437d3aef92a552a84a24ed2a806536c0a80fd7
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-29 23:35:38 +0300

    squash!

---
+++ src/options/package.ts
@@ -0,0 +1,49 @@
+import css3Dark from "@assets/images/packages/css3Dark.svg";
+import css3Light from "@assets/images/packages/css3Light.svg";
+import gnubashDark from "@assets/images/packages/gnubashDark.svg";
+import gnubashLight from "@assets/images/packages/gnubashLight.svg";
+import goDark from "@assets/images/packages/goDark.svg";
+import goLight from "@assets/images/packages/goLight.svg";
+import javascriptDark from "@assets/images/packages/javascriptDark.svg";
+import javascriptLight from "@assets/images/packages/javascriptLight.svg";
+import luaDark from "@assets/images/packages/luaDark.svg";
+import luaLight from "@assets/images/packages/luaLight.svg";
+import mdxDark from "@assets/images/packages/mdxDark.svg";
+import mdxLight from "@assets/images/packages/mdxLight.svg";
+import powershellDark from "@assets/images/packages/powershellDark.svg";
+import powershellLight from "@assets/images/packages/powershellLight.svg";
+import pythonDark from "@assets/images/packages/pythonDark.svg";
+import pythonLight from "@assets/images/packages/pythonLight.svg";
+import rustDark from "@assets/images/packages/rustDark.svg";
+import rustLight from "@assets/images/packages/rustLight.svg";
+import typescriptDark from "@assets/images/packages/typescriptDark.svg";
+import typescriptLight from "@assets/images/packages/typescriptLight.svg";
+import windowsterminalDark from "@assets/images/packages/windowsterminalDark.svg";
+import windowsterminalLight from "@assets/images/packages/windowsterminalLight.svg";
+
+export default {
+	icons: {
+		css3Dark,
+		css3Light,
+		gnubashDark,
+		gnubashLight,
+		goDark,
+		goLight,
+		javascriptDark,
+		javascriptLight,
+		luaDark,
+		luaLight,
+		mdxDark,
+		mdxLight,
+		powershellDark,
+		powershellLight,
+		pythonDark,
+		pythonLight,
+		rustDark,
+		rustLight,
+		typescriptDark,
+		typescriptLight,
+		windowsterminalDark,
+		windowsterminalLight,
+	},
+};
