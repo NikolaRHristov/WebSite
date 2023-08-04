3bc7ae55eda15b72d048649d4d02b6a1a344093d
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-04 20:11:38 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import worker from "astrojs-service-worker";
 export default defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
+	outDir: "./Build",
 	site: "https://nikolahristov.tech/",
 	experimental: {
 		assets: true,
