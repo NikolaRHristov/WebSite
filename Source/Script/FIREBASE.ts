006ddd865ea6c0e88aff4c8e3715867853daa66a
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-06 01:44:52 +0300

    squash!

--- Source/Script/FIREBASE.ts
+++ Source/Script/FIREBASE.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp as Initialize } from "firebase/app";
 
-const firebaseConfig = {
+Initialize({
 	apiKey: "AIzaSyCiIOIiAvdRwC5n6AzX_F5WSivt9bvosTQ",
 	authDomain: "nikolahristov-web.firebaseapp.com",
 	projectId: "nikolahristov-web",
@@ -8,6 +8,4 @@ const firebaseConfig = {
 	messagingSenderId: "540449914377",
 	appId: "1:540449914377:web:bf0a39a757e9ebcd1ec2d8",
 	measurementId: "G-146FB9N18L",
-};
-
-initializeApp(firebaseConfig);
+});
