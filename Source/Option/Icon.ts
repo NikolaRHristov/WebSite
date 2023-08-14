41041837c898d0107af3ce50a23ddc161f06b68a
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-14 15:33:23 +0300

    squash!

--- Source/Option/Icon.ts
+++ Source/Option/Icon.ts
@@ -23,25 +23,21 @@ import typescriptLight from "@Asset/Image/Package/typescriptLight.svg";
 import windowsterminalDark from "@Asset/Image/Package/windowsterminalDark.svg";
 import windowsterminalLight from "@Asset/Image/Package/windowsterminalLight.svg";
 
-export interface Package {
-	icons: {
-		[key: string]: [ImageMetadata, ImageMetadata];
-	};
+export interface Icon {
+	[key: string]: [ImageMetadata, ImageMetadata];
 }
 
 export default {
-	icons: {
-		"CSS": [css3Light, css3Dark],
-		"Shell": [gnubashLight, gnubashDark],
-		"Go": [goLight, goDark],
-		"JavaScript": [javascriptLight, javascriptDark],
-		"Lua": [luaLight, luaDark],
-		"MDX": [mdxLight, mdxDark],
-		"PowerShell": [powershellLight, powershellDark],
-		"Python": [pythonLight, pythonDark],
-		"Rust": [rustLight, rustDark],
-		"TypeScript": [typescriptLight, typescriptDark],
-		"Batchfile": [windowsterminalLight, windowsterminalDark],
-		"Astro": [astroLight, astroDark],
-	},
-} satisfies Package as Package;
+	"CSS": [css3Light, css3Dark],
+	"Shell": [gnubashLight, gnubashDark],
+	"Go": [goLight, goDark],
+	"JavaScript": [javascriptLight, javascriptDark],
+	"Lua": [luaLight, luaDark],
+	"MDX": [mdxLight, mdxDark],
+	"PowerShell": [powershellLight, powershellDark],
+	"Python": [pythonLight, pythonDark],
+	"Rust": [rustLight, rustDark],
+	"TypeScript": [typescriptLight, typescriptDark],
+	"Batchfile": [windowsterminalLight, windowsterminalDark],
+	"Astro": [astroLight, astroDark],
+} satisfies Icon as Icon;
