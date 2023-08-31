eac9cf2ed33680af73fc60d4c0f401b857d30224
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-31 12:55:56 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -11,10 +11,6 @@ export default defineConfig({
 	publicDir: "./Public",
 	outDir: "./Target",
 	site: "https://nikolahristov.tech/",
-	experimental: {
-		assets: true,
-		viewTransitions: true,
-	},
 	compressHTML: true,
 	integrations: [
 		import.meta.env.MODE === "production" ? Worker() : null,
