37d3511a06aa4b154a70c489d0c856ba26fc3664
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-13 13:00:58 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,4 +1,6 @@
-(await import("firebase/app")).initializeApp({
+import { initializeApp } from "firebase/app";
+
+initializeApp({
 	apiKey: "AIzaSyCiIOIiAvdRwC5n6AzX_F5WSivt9bvosTQ",
 	authDomain: "nikolahristov-web.firebaseapp.com",
 	projectId: "nikolahristov-web",
@@ -7,5 +9,3 @@
 	appId: "1:540449914377:web:bf0a39a757e9ebcd1ec2d8",
 	measurementId: "G-146FB9N18L",
 });
-
-export {};
