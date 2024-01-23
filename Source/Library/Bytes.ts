f52a94feb324e42fad84837d8e83f51e6d85b4e3
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-23 14:39:42 +0200

    squash!

--- Source/Library/Bytes.ts
+++ Source/Library/Bytes.ts
@@ -7,7 +7,7 @@ export default async (Bytes: number, Decimals = 2.0) => {
 
 	const I = Math.floor(Math.log(Bytes) / Math.log(K));
 
-	return `${parseFloat(
+	return `${Number.parseFloat(
 		(Bytes / K ** I).toFixed(Decimals < 0 ? 0 : Decimals),
 	)} ${["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][I]}`;
 };
