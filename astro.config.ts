7d8a07db660cce634ad6dafe6444a3569878d228
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-30 17:15:16 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ export default (await import("astro/config")).defineConfig({
 	outDir: "./Target",
 	site: "https://nikolahristov.tech/",
 	compressHTML: true,
+	output: "server",
 	integrations: [
 		import.meta.env.MODE === "production"
 			? (await import("astrojs-service-worker")).default()
