7cad334c28fed24a5adb19581e61d6f8b84d8c1d
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-05 04:23:32 +0200

    squash!

--- Source/Library/Bytes.ts
+++ Source/Library/Bytes.ts
@@ -1,13 +1,19 @@
-export default async (Bytes: number, Decimals = 2.0) => {
+/**
+ * @module Bytes
+ *
+ */
+export default ((...[Bytes, Decimals = 2.0]: Parameters<Type>) => {
 	if (Bytes === 0) {
 		return "0 Bytes";
 	}
 
-	const K = 1024;
-
-	const I = Math.floor(Math.log(Bytes) / Math.log(K));
+	const I = Math.floor(Math.log(Bytes) / Math.log(Kilobyte));
 
 	return `${Number.parseFloat(
-		(Bytes / K ** I).toFixed(Decimals < 0 ? 0 : Decimals),
+		(Bytes / Kilobyte ** I).toFixed(Decimals < 0 ? 0 : Decimals)
 	)} ${["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][I]}`;
-};
+}) satisfies Type as Type;
+
+import type Type from "../Interface/Bytes.js";
+
+export const Kilobyte = 1024;
