4ca4f8670a641c205036ca2f93c983004ca9d121
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-09 05:32:59 +0300

    squash!

---
+++ Target/_astro/Base.astro_astro_type_script_index_0_lang.DPUg3DoF.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./preload-helper.BiBI96sQ.js";(await e(()=>import("./index.esm.DY3F5bZw.js"),[])).initializeApp({apiKey:"AIzaSyCiIOIiAvdRwC5n6AzX_F5WSivt9bvosTQ",authDomain:"nikolahristov-web.firebaseapp.com",projectId:"nikolahristov-web",storageBucket:"nikolahristov-web.appspot.com",messagingSenderId:"540449914377",appId:"1:540449914377:web:bf0a39a757e9ebcd1ec2d8",measurementId:"G-146FB9N18L"});
+//# sourceMappingURL=Base.astro_astro_type_script_index_0_lang.DPUg3DoF.js.map
