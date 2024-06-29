ba10e48db82573b99af36cedee3222d6a3a9829c
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-29 09:50:10 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- astro.config.ts
+++ astro.config.ts
@@ -5,22 +5,22 @@ export default (await import("astro/config")).defineConfig({
 	site: "https://nikolahristov.tech/",
 	compressHTML: true,
 	integrations: [
-		// // @ts-ignore
-		// import.meta.env.MODE === "production"
-		// 	? (await import("astrojs-service-worker")).default()
-		// 	: null,
-		// (await import("@astrojs/sitemap")).default(),
-		// (await import("@playform/inline")).default({ Logger: 1 }),
-		// (await import("@astrojs/prefetch")).default(),
-		// (await import("@playform/format")).default({ Logger: 1 }),
-		// (await import("@playform/compress")).default({
-		// 	Logger: 1,
-		// 	Exclude: [(File: string) => File.indexOf("Raw") !== -1],
-		// 	CSS: {
-		// 		// @ts-expect-error
-		// 		lightningcss: false,
-		// 	},
-		// }),
+		// @ts-ignore
+		import.meta.env.MODE === "production"
+			? (await import("astrojs-service-worker")).default()
+			: null,
+		(await import("@astrojs/sitemap")).default(),
+		(await import("@playform/inline")).default({ Logger: 1 }),
+		(await import("@astrojs/prefetch")).default(),
+		(await import("@playform/format")).default({ Logger: 1 }),
+		(await import("@playform/compress")).default({
+			Logger: 1,
+			Exclude: [(File: string) => File.indexOf("Raw") !== -1],
+			CSS: {
+				// @ts-expect-error
+				lightningcss: false,
+			},
+		}),
 	],
 	experimental: {
 		directRenderScript: true,
