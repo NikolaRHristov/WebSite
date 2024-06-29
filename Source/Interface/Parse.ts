fc64b19802d70b46b4e65e64d222832b62c92f0c
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-29 05:12:45 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Interface/Parse.ts
+++ Source/Interface/Parse.ts
@@ -6,5 +6,5 @@ export default interface Interface {
 	(Packages: Set<Cell>): Promise<Set<PackagesRowItem>>;
 }
 
-import type PackagesRowItem from "../Interface/PackagesRowItem";
-import type Cell from "../Type/Cell";
+import type PackagesRowItem from "../Interface/PackagesRowItem.js";
+import type Cell from "../Type/Cell.js";
