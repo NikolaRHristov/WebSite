a54da32002eb27ea5001c7585ec5d3edeb716bcc
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-01 06:10:00 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ export default (await import("astro/config")).defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
 	outDir: "./Target",
-	site: "https://nikolahristov.tech/",
+	site: "https://nikolahristov.tech",
 	compressHTML: true,
 	integrations: [
 		// @ts-ignore
